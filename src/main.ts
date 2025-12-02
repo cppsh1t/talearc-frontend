3bb8dd60948a314a8fd8e3d61690e5dc9be2b5a6
unknown <hong.yuan@tslsmart.com>
2025-12-02 09:48:53 +0800

    feat: 添加依赖

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import '@/style/main.css'
+import 'virtual:uno.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,57 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import UnoCSS from 'unocss/vite'
+import postcssPxToViewport from 'postcss-px-to-viewport-8-plugin'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+const devServer = 'http://10.37.126.117:10315/'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vueDevTools(),
+    // vueDevTools(),
+    UnoCSS(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'talearc-dist',
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcssPxToViewport({
+          viewportWidth: 1920,
+          viewportHeight: 1080,
+          unitPrecision: 6,
+          viewportUnit: 'vw',
+          selectorBlackList: [],
+          minPixelValue: 1,
+          mediaQuery: false,
+        }),
+      ],
+    },
+  },
+  server: {
+    port: 3879,
+    proxy: {
+      '/talearc': {
+        target: devServer,
+      },
     },
   },
 })
